53764f6d24cde61eff001650cfa4c3f1ff13df60
Harris <72910229+harriscreasy@users.noreply.github.com>
2020-12-10 14:27:01 +0100

    Ajout du slider et réglage des cards

---
+++ script/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+function ouvrirPage(){
+    var search = document.querySelector(".form-control");
+}
